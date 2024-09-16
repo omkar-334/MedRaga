b32f43588bf97d870e1883ef5f21028b04901ab7
omkar-334 <omkarkabde@gmail.com>
2024-09-16 09:08:02 +0530

    lc -> li

--- app.py
+++ app.py
@@ -7,22 +7,23 @@ import time
 
 import cohere
 import requests
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 from fastapi import APIRouter, Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from langchain.prompts import ChatPromptTemplate
-from langchain_cohere import CohereEmbeddings
-from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
-from langchain_community.vectorstores import Qdrant
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from llama_index.core import SimpleDirectoryReader, StorageContext, VectorStoreIndex
+from llama_index.core.node_parser import MarkdownNodeParser
+from llama_index.embeddings.cohere import CohereEmbedding
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.groq import Groq
+from llama_index.vector_stores.qdrant import QdrantVectorStore
+
 from mthrottle import Throttle
-from playwright.async_api import async_playwright
 from pydantic import BaseModel
 from pypdf import PdfReader
 from pypdf.errors import PdfReadError
 from qdrant_client import QdrantClient
 from qdrant_client.models import Batch, Distance, VectorParams
+from serpapi import GoogleSearch
 
 logger = logging.getLogger("PyPDF2")
 logger.setLevel(logging.ERROR)
@@ -30,34 +31,20 @@ logger.setLevel(logging.ERROR)
 throttle_config = {"lookup": {"rps": 15}, "default": {"rps": 8}}
 th = Throttle(throttle_config, 15)
 
-llm15 = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest", temperature=0.9)
-llm1 = ChatGoogleGenerativeAI(model="gemini-1.0-pro-latest")
+apidict = dict(dotenv_values())
 
-load_dotenv()
-COHERE_API_KEY = os.environ["COHERE_API_KEY"]
-GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
-LANGCHAIN_TRACING_V2 = os.environ["LANGCHAIN_TRACING_V2"]
-LANGCHAIN_ENDPOINT = os.environ["LANGCHAIN_ENDPOINT"]
-LANGCHAIN_API_KEY = os.environ["LANGCHAIN_API_KEY"]
-LANGCHAIN_PROJECT = os.environ["LANGCHAIN_PROJECT"]
+llm = Groq(model="llama3-70b-8192", api_key="your_api_key")
 
 
-async def get_google(condition, newpath, n):
-    prompts = [i.format(condition) for i in ["{} causes", "{} risk factors", "{} symptoms", "{} prevention and cure", "{} diagnosis", "{} treatment options", "{} prognosis", "{} complications", "{} epidemiology", "{} research and studies", "{} latest treatments and advancements"]]
+async def get_links(condition, n):
+    queries = [i.format(condition) for i in ["{} causes", "{} risk factors", "{} symptoms", "{} prevention and cure", "{} diagnosis", "{} treatment options", "{} prognosis", "{} complications", "{} epidemiology", "{} research and studies", "{} latest treatments and advancements"]]
 
     output = []
-    async with async_playwright() as p:
-        browser = await p.chromium.launch()
-        page = await browser.new_page()
-        for prompt in prompts:
-            url = f"https://www.google.com/search?q={prompt}&num={n}&hl=en&btnG=Google+Search&as_filetype=pdf"
-            await page.goto(url)
-
-            xpath = "//div[@class='yuRUbf']//a[contains(@href, 'pdf')]"
-            links = await page.query_selector_all(xpath)
-            links = await asyncio.gather(*(link.get_attribute("href") for link in links))
-            output.extend(links)
-        await browser.close()
+    for query in queries:
+        params = {"q": query, "num": n, "hl": "en", "source": "python", "serp_api_key": apidict["SERPAPI_KEY"]}
+        search = GoogleSearch(params).get_dict()
+        links = [i["link"] for i in search["organic_results"]]
+        output.extend(links)
     return output
 
 
@@ -105,46 +92,37 @@ def clean_dir(directory):
                 os.remove(filepath)
     print("Directory cleaned")
 
+def create_collection(name):
+    client = QdrantClient(url="http://localhost:6333")
+    client.create_collection(
+        collection_name="MedicalPapers",
+        vectors_config=VectorParams(size=1024, distance=Distance.DOT),
+    )
+
+def embed(textlist):
+    cohere_client = cohere.Client(COHERE_API_KEY)
+    cohere_client.embed(
+                model="embed-english-v3.0",
+                input_type="search_document",
+                texts=textlist,
+            )
 
 def embed_docs(path):
-    loader = DirectoryLoader(path, glob="**/*.pdf", loader_cls=PyPDFLoader, silent_errors=True)
-    pages = loader.load_and_split()
+    reader = SimpleDirectoryReader(input_dir=path)
+    documents = reader.load_data()
+
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=100, length_function=len, is_separator_regex=False)
     texts = text_splitter.create_documents([pages[i].page_content for i in range(len(pages))])
-    client = QdrantClient(url="http://localhost:6333")
-    # client.create_collection(
-    #     collection_name="MedicalPapers",
-    #     vectors_config=VectorParams(size=1024, distance=Distance.DOT),
-    # )
     # qdrant = Qdrant.from_documents(docs, embeddings, url=QDRANT_URL, collection_name=QDRANT_CLUSTER, api_key=QDRANT_KEY, force_recreate=True)
-    cohere_client = cohere.Client(COHERE_API_KEY)
     client = QdrantClient()
-    half = len(texts) // 2
     client.upsert(
         collection_name="MedicalPapers",
         points=Batch(
-            ids=range(half),
-            vectors=cohere_client.embed(
-                model="embed-english-v3.0",
-                input_type="search_document",
-                texts=[texts[i].page_content for i in range(half)],
-            ).embeddings,
-            payloads=[{"Context{}".format(index): value} for index, value in enumerate([texts[i].page_content for i in range(half)], start=1)],
+            ids=range(len(texts)),
+            vectors=.embeddings,
+            payloads=[{"Context{}".format(index): value} for index, value in enumerate([texts[i].page_content for i in range(len(texts))], start=1)],
         ),
     )
-    client.upsert(
-        collection_name="MedicalPapers",
-        points=Batch(
-            ids=range(half, len(texts)),
-            vectors=cohere_client.embed(
-                model="embed-english-v3.0",
-                input_type="search_document",
-                texts=[texts[i].page_content for i in range(half, len(texts))],
-            ).embeddings,
-            payloads=[{"Context{}".format(index): value} for index, value in enumerate([texts[i].page_content for i in range(half, len(texts))], start=1)],
-        ),
-    )
-    print("Documents indexed and embedded")
 
 
 def cohereRetrival(collection, textList):
@@ -171,17 +149,13 @@ def ragFusion(prompt, collection="MedicalPapers"):
         ret = cohereRetrival(collection, [query])
         for doc in ret:
             for key, value in doc.payload.items():
-                value = value.replace("\xa0", " ")
-                value = value.replace("\t", "  ")
-                value = value.replace("\r", "")
-                value = value.replace("\n", "      ")
+                value = value.replace("\xa0", " ").replace("\t", "  ").replace("\r", "").replace("\n", "      ")
                 retrievedContent.append(value)
     retrievedContent = list(set(retrievedContent))
     result = co.rerank(model="rerank-english-v3.0", query=prompt, documents=retrievedContent, top_n=5, return_documents=True)
     context = ""
     for i in result.results:
-        context += i.document.text
-        context += "\n\n"
+        context += i.document.text + "\n\n"
     return context
 
 
@@ -195,7 +169,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-router = APIRouter()
 
 
 class CParams(BaseModel):
@@ -209,7 +182,7 @@ class QParams(BaseModel):
     # prompt: str
 
 
-@router.post("/create")
+@app.post("/create")
 async def create(params=Depends(CParams)):
     start = time.time()
     req = json.loads(params.req)
@@ -239,7 +212,7 @@ async def create(params=Depends(CParams)):
     return {"id": id, "status": f"Downloaded {length} files", "Path": newpath, "Time taken": time.time() - start}
 
 
-@router.get("/query")
+@app.get("/query")
 async def query(params=Depends(QParams)):
     treatment_box = """You are a medical assistant that specializes in providing second opinions, diagnosing complex cases and suggesting treatment plans. When I describe the patient details, medical context and task, give me the appropriate treatment plan based on the task given by analyzing the patient details and medical context. Include how your answer is related to the patient's history. Do not print the analysis or summary of the patient's details."""
     answer_box = """
@@ -291,11 +264,6 @@ As a medical assistant specializing in second opinions, treatment plans and medi
     return {"Output": result.content}
 
 
-@router.get("/status")
+@app.get("/status")
 def status():
     return {"status": "200 OK"}
-
-
-app.include_router(router)
-
-print("helo")
